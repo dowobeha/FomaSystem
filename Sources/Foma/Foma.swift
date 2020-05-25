e25fec61c89664b90dce19e0643aa09457889538
Lane Schwartz <dowobeha@gmail.com>
2020-05-25 13:51:24 -0500

    Implementation

--- Sources/Foma/Foma.swift
+++ Sources/Foma/Foma.swift
@@ -1,6 +1,69 @@
 import CFomaSystem
 
-struct Foma {
-    let version = String(cString: fsm_get_library_version_string())
+extension String {
+    
+    func unsafeMutablePointer() -> UnsafeMutablePointer<CChar> {
+        let cString: UnsafeMutablePointer<CChar> = self.withCString { (cString:UnsafePointer<CChar>) in
+            return UnsafeMutablePointer<CChar>(mutating: cString)
+        }
+        return cString
+    }
+    
 }
 
+struct Foma {
+    static let version = String(cString: fsm_get_library_version_string())
+}
+
+class FSM {
+    private var fsmPointer : UnsafeMutablePointer<fsm>
+    private var applyHandle: UnsafeMutablePointer<apply_handle>
+    
+    init(fromBinary binaryFilename: String) {
+        self.fsmPointer = fsm_read_binary_file(binaryFilename.unsafeMutablePointer())
+        self.applyHandle = apply_init(fsmPointer)
+    }
+    
+    private init(fromPointer unsafeMutablePointer: UnsafeMutablePointer<fsm>) {
+        self.fsmPointer = unsafeMutablePointer
+        self.applyHandle = apply_init(fsmPointer)
+    }
+    
+    deinit {
+        apply_clear(self.applyHandle)
+        fsm_destroy(self.fsmPointer)
+    }
+    
+    func applyUp(_ s: String) -> String {
+        let result: UnsafeMutablePointer<CChar> = apply_up(self.applyHandle, s.unsafeMutablePointer())
+        return String(cString: result)
+    }
+
+    func applyDown(_ s: String) -> String {
+        let result: UnsafeMutablePointer<CChar> = apply_down(self.applyHandle, s.unsafeMutablePointer())
+        return String(cString: result)
+    }
+    
+    func randomLower() -> String {
+        let result: UnsafeMutablePointer<CChar> = apply_random_lower(self.applyHandle)
+        return String(cString: result)
+    }
+
+    func randomUpper() -> String {
+        let result: UnsafeMutablePointer<CChar> = apply_random_upper(self.applyHandle)
+        return String(cString: result)
+    }
+    
+    func numStates() -> Int {
+        let readHandle: UnsafeMutablePointer<fsm_read_handle> = fsm_read_init(self.fsmPointer)
+        defer {
+            fsm_read_done(readHandle)
+        }
+        return Int(fsm_get_num_states(readHandle))
+    }
+    
+    func union(_ other: FSM) -> FSM {
+        return FSM(fromPointer: fsm_union(self.fsmPointer, other.fsmPointer))
+    }
+    
+}
--- Tests/FomaTests/FomaTests.swift
+++ Tests/FomaTests/FomaTests.swift
@@ -6,7 +6,12 @@ final class FomaTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Foma().version, "0.9.18alpha")
+        XCTAssertEqual(Foma.version, "0.9.18alpha")
+    }
+
+    func testFomaReadBinaryFile() {
+        let fsm = FSM(fromBinary: "/Users/lanes/work/summer/yupik/yupik-foma-v2/lower.fomabin")
+        XCTAssertEqual(fsm.applyUp("qikmiq"), "qikmigh(N)^[Abs.Sg]")
     }
 
     static var allTests = [
