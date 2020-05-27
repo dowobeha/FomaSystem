2bf6e805ce654b9ece65e0ef2af86e20eafc09d8
Lane Schwartz <dowobeha@gmail.com>
2020-05-27 16:47:24 -0500

    Handle linker flags correctly in Package.swift

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "Foma",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
+        Product.library(
             name: "Foma",
             targets: ["Foma"]),
     ],
@@ -18,13 +18,15 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .systemLibrary(
-	    name: "CFomaSystem"),
-	.target(
+        Target.systemLibrary(
+            name: "CFomaSystem"),
+        Target.target(
             name: "Foma",
-            dependencies: ["CFomaSystem"]),
-        .testTarget(
+            dependencies: ["CFomaSystem"],
+            linkerSettings: [LinkerSetting.unsafeFlags(["-Xlinker", "-L/usr/local/lib"])]),
+        Target.testTarget(
             name: "FomaTests",
-            dependencies: ["Foma"]),
+            dependencies: ["Foma"],
+            linkerSettings: [LinkerSetting.unsafeFlags(["-Xlinker", "-L/usr/local/lib"])]),
     ]
 )
